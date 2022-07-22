dac9a7865b783a0fb1c26e8aabd083e80381a7a5
Duy Pham <ericpham19@outlook.com>
2022-07-22 16:27:13 +1000

    finish lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+
+const pets = ["Milo", "Otis", "Garfield"];
+
+function append() {
+   pets.append("Odie")
+}
+function prepend(){
+    pets.prepend("Odie");
+}
+
+function removeLast(){
+    pets.slice(0, pets.length-1);   
+}
+
+function removeFirst(){
+    pets.slice(1);
+}
